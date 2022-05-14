8423fa64aa5cdc642bfc401ff0a4266b248f4084
Vivek <patelvivek1126@gmail.com>
2022-05-13 23:39:34 -0400

    Adding openmp tree depth and configs

--- openmp/tree_depth.cpp
+++ openmp/tree_depth.cpp
@@ -1,19 +1,35 @@
 #include <omp.h>
+#include "../openmp/helper.cpp"
+#include <algorithm>
 
 #ifndef DEPTH_
 #define DEPTH_
 
 
-int tree_depth()
+int tree_depth(node *n)
 {
-    //TODO: Implement recursive parallel tree-depth.
-    return 0;
+    if(n -> is_leaf){
+        return 0;
+    }
+    else{
+        int d1, d2;
+
+        #pragma omp task shared(d1)
+        d1 = tree_depth(n->left_child);
+
+        #pragma omp task shared(d2)
+        d2 = tree_depth(n->right_child);
+
+        #pragma omp taskwait
+
+        n->depth = 1 + std::max(d1, d2);
+        return n->depth;
+    }
 }
 
-int compute_depth()
+int compute_depth(node *root)
 {
-    //TODO: Implement compute depth.
-    return 0;
+    return tree_depth(root);
 }
 
 #endif
