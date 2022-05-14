84176840bda44dd402feb301c38ed06995fd0f67
Vivek <patelvivek1126@gmail.com>
2022-05-14 00:24:42 -0400

    Starting pthreads

--- pthreads/compute_avg.cpp
+++ pthreads/compute_avg.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <cassert>
+#include <pthread.h>
+#include <cmath>
+#include "../pthreads/helper.cpp"
 
 int main(int argc, char *argv[]){
-    //TODO: compute parallel average
+    if(argc < 3) {
+        fprintf(stderr, "Must specify the size of the array and number of threads.\n");
+        exit(1);
+    }
+    if(argc > 3) {
+        srand(atoi(argv[3]));
+    }
+
+    unsigned array_size = atoi(argv[1]);
+    int num_threads = atoi(argv[2]);
+    thread_info_struct thread_info[num_threads];
+    int* A = (int *)malloc(array_size * sizeof(int));
+    assert(A != NULL);
+
+    //TODO - Initialilze array
+
+    //TODO - create thread to compute average by dividing into chunks
+
+    //Join threads
+
+    //To be continued...
     return 0;
 }
