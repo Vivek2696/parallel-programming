61e47705ae2864c4509da9105c78efdee10353c9
Vivek <patelvivek1126@gmail.com>
2022-07-05 12:49:48 -0400

    Finalizing MPI reduce code

--- mpi/main.cpp
+++ mpi/main.cpp
@@ -1,6 +1,28 @@
 #include "../mpi/reduce.cpp"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    //TODO: Use reduce.cpp for mpi
+    MPI_Init(&argc, &argv);
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    if(argc > 1){
+        srand(atoi(argv[1])+my_rank);
+    }    
+
+    int local_val = rand() % 500;
+    fprintf(stderr, "rank %d has local value of %i\n", my_rank, local_val);
+    int sum = 0;
+
+    my_reduce(&local_val, &sum);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    if(my_rank == 0){
+        fprintf(stdout, "Sum is %d\n",sum);
+    }
+
+    MPI_Finalize();
+
     return 0;
 }
--- mpi/reduce.cpp
+++ mpi/reduce.cpp
@@ -10,5 +10,32 @@ void my_reduce(int *my_val, int *sum_val){
     leftchild = 2 * my_rank + 1;
     rightchild = 2 * my_rank + 2;
 
-    //TODO: Reduce sum_val up the tree to the root node
+    if(leftchild < nprocs){
+        int left_sum = 0;
+        MPI_Recv(&left_sum, 1, MPI_INT, leftchild, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+        *sum_val += left_sum;
+    }
+
+    if(rightchild < nprocs){
+        int right_sum = 0;
+        MPI_Recv(&right_sum, 1, MPI_INT, rightchild, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+        *sum_val += right_sum;
+    }
+
+    if(my_rank % 2 == 0){
+        parent = (my_rank - 2) / 2;
+    }
+    else{
+        parent = (my_rank - 1) / 2;
+    }
+
+    if(parent >= 0){
+        *sum_val += *my_val;
+        MPI_Send(sum_val, 1, MPI_INT, parent, 0, MPI_COMM_WORLD);
+    }
+
+    if(my_rank == 0){
+        *sum_val += *my_val;
+    }
+
 }
